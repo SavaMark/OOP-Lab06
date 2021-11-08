58d3dfc8f742be5ac3ac4277214986ac483169d5
MARCO_SAVARESE <marco.savarese@studio.unibo.it>
2021-11-08 08:44:14 +0100

    More testing on Collections1 functions except map

--- src/it/unibo/oop/lab/collections1/UseCollection.java
+++ src/it/unibo/oop/lab/collections1/UseCollection.java
@@ -1,11 +1,24 @@
 package it.unibo.oop.lab.collections1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Example class using {@link java.util.List} and {@link java.util.Map}.
  * 
  */
 public final class UseCollection {
-
+	
+	private static final int TIMES = 1000;
+	private static final int TO_MS = 1_000_000;
+	private static final int RANGE_MIN = 1000;
+	private static final int RANGE_MAX = 2000;
+	private static final int ELEMS = 100000;
+	
     private UseCollection() {
     }
 
@@ -14,36 +27,94 @@ public final class UseCollection {
      *            unused
      */
     public static void main(final String... s) {
-        /*
-         * 1) Create a new ArrayList<Integer>, and populate it with the numbers
-         * from 1000 (included) to 2000 (excluded).
-         */
-        /*
-         * 2) Create a new LinkedList<Integer> and, in a single line of code
-         * without using any looping construct (for, while), populate it with
-         * the same contents of the list of point 1.
-         */
-        /*
-         * 3) Using "set" and "get" and "size" methods, swap the first and last
-         * element of the first list. You can not use any "magic number".
-         * (Suggestion: use a temporary variable)
-         */
-        /*
-         * 4) Using a single for-each, print the contents of the arraylist.
-         */
+    	final List<Integer> list = new ArrayList<>();
+    	for(int i = RANGE_MIN; i < RANGE_MAX; i++) {
+    		list.add(i);
+    	}
+    	System.out.println(list);
+    	
+    	
+    	final List<Integer> list2 = new ArrayList<Integer>();
+    	list2.addAll(list);
+    	System.out.println(list2);
+        
+    	
+    	int last = list.size() - 1;
+    	int count = 0;
+    	int tmp;
+    	tmp = list.get(count);
+		list.set(count, list.get(last));
+		list.set(last, tmp);
+    	System.out.println(list);
+    	
+    	
+    	for(int elem: list) {
+    		System.out.println(elem);
+    	}
+    	
+    	
+    	
+    	final List<Integer> sacrificio1 = new ArrayList<Integer>();
+    	final List<Integer> sacrificio2 = new LinkedList<Integer>();
+    	
+    	long time = System.nanoTime();
+    	for(int i = 0; i < ELEMS; i++) {
+    		sacrificio1.add(0, i);
+    	}
+    	time = System.nanoTime() - time;
+    	System.out.println("Adding " + ELEMS
+                + " elements in ArrayList took " + time
+                + " ns (" + time / TO_MS + "ms)");
+    	
+    	
+    	time = System.nanoTime();
+    	for(int i = 0; i < ELEMS; i++) {
+    		sacrificio2.add(0, i);
+    	}
+    	time = System.nanoTime() - time;
+    	System.out.println("Adding " + ELEMS
+                + " elements in LinkdList took " + time
+                + "   ns (" + time / TO_MS + "  ms)");
+    	
+    	
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
          * first element of the collection for both ArrayList and LinkedList,
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
+         * 
+         * LINKED LIST PIU VELOCE DI ARRAY LIST
          */
+    	
+    	time = System.nanoTime();
+    	for(int i = 0; i < TIMES; i++) {
+    		sacrificio1.get(50000);
+    	}
+    	time = System.nanoTime() - time;
+    	System.out.println(time);
+    	
+    	time = System.nanoTime();
+    	for(int i = 0; i < TIMES; i++) {
+    		sacrificio2.get(50000);
+    	}
+    	time = System.nanoTime() - time;
+    	System.out.println(time);
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
+         * 
+         * ARRAY LIST MOLTO PIU VELOCE DI LINKED LIST
+         * 
          */
+    	
+    	final Map<String, Integer> map = new HashMap<String, Integer>();
+    	map.put("Africa", 1_110_635_000);
+    	map.put("Americas", 972_005_000);
+    	map.put("Antarctica", 0);
+    	
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
